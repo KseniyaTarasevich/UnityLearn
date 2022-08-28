ee2e6e6ed4bade701e404b5d062861c9a6a12d12
Kseniya Tarasevich <tarasevichkseniya11@gmail.com>
2022-08-28 23:43:22 +0300

    add player input for forward/backward movements and right/left add empty GameObject for all obstacles add some comments in the PlayerController.cs

--- UnityLearn/Create with code/Prototype1/Assets/Scripts/PlayerController.cs
+++ UnityLearn/Create with code/Prototype1/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10f;
-    public float turnSpeed;
+    private float speed = 20f;
+    private float turnSpeed = 45f;
+    private float horizontalInput;
+    private float forwardInput;
     void Start()
     {
 
@@ -13,7 +13,12 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed); ;
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // Moves the car forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up * horizontalInput * Time.deltaTime * turnSpeed);
     }
 }
