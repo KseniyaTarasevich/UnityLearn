2bc2ab71f715b3a4794168382e2c42a08a950faa
KseniyaTarasevich <tarasevichkseniya11@gmail.com>
2022-09-05 16:45:29 +0300

    start new tutorial

---
+++ UnityLearn/Create with code/Prototype2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+    }
+}
