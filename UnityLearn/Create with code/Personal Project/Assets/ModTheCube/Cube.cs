c34a0882fd6c2793b49759f0ce6365c96ba9425d
KseniyaTarasevich <tarasevichkseniya11@gmail.com>
2022-09-19 12:25:03 +0300

    trying to build and publish WebGL project

---
+++ UnityLearn/Create with code/Personal Project/Assets/ModTheCube/Cube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+
+    void Start()
+    {
+        transform.position = new Vector3(3, 4, 1);
+        transform.localScale = Vector3.one * 2f;
+
+        Material material = Renderer.material;
+
+        material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+    }
+
+    void Update()
+    {
+        transform.Rotate(25.0f * Time.deltaTime, 0f, 5f * Time.deltaTime);
+    }
+}
