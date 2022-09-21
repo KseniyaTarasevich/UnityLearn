380ec13460fb8c07d07831200eee0d236da2b5d7
KseniyaTarasevich <tarasevichkseniya11@gmail.com>
2022-09-21 10:36:59 +0300

    add player animation

--- UnityLearn/Create with code/Prototype3/Assets/Scripts/PlayerController.cs
+++ UnityLearn/Create with code/Prototype3/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+    private Animator animPlayer;
     public float jumpForce;
     public float gravityModifier;
     public bool isOnGround = true;
@@ -13,16 +14,18 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        animPlayer = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            animPlayer.SetTrigger("Jump_trig");
         }
     }
 
@@ -32,10 +35,12 @@ public class PlayerController : MonoBehaviour
         {
             isOnGround = true;
         }
-        else if(collision.gameObject.CompareTag("Obstacle"))
+        else if (collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
             Debug.Log("Game Over!");
+            animPlayer.SetBool("Death_b", true);
+            animPlayer.SetInteger("DeathType_int", 1);
         }
     }
 }
